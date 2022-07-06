e79e27188206c7505bc1cee93b12edc8cdd5ef1e
TobbsProjects <tobsson89@gmail.com>
2022-07-06 17:09:35 +0200

    Structure set
    The structure is set, eventlistener added to the arrows, front page is done.

---
+++ script.js
@@ -0,0 +1,18 @@
+const informationHeader = document.getElementById("informationHead").innerHTML;
+const informationText = document.getElementById("informationText").innerHTML;
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+
+const aboutHead = "Kort om mig";
+const aboutText = "Tobias heter jag och bor i Ludvika med fru och 4 barn. Med tanke på antalet barn så blir det inte så mycket PC för mig på fritiden, men nu har jag hittat en ursäkt som min fru godtar, koda! Jag har alltid suttit vid en dator. Det började med att man spelade SkiFree på pappas laptop. Sedan har intresset fortsatt med diverse spel, musik, film och allt annat roligt man kan göra på en dator. Detta ledde mig till att söka den gymnasielinje, som jag kunde söka i närheten av vart jag bodde och där mina poäng räckte till; El- dator. Skoltröttheten tog över, men datorintresset har alltid legat latent. Har försökt lära mig koda vid tidigare tillfällen, men livet har kommit emellan. Men nu är det dags på riktigt! Har studerat i c:a en månad och har en utstansad linje för att till slut landa i full-stack. Efter det ligger även C# och C++ som språk som jag vill lära mig tillsammans med spelmotorerna Unity och Unreal, men det hör till fritiden. Målsättningen är att få ett jobb så snart som möjligt för att finslipa det som jag kommer att lära mig nu under sommaren.";
+
+function changeLeft() {
+    
+}
+
+function changeRight() {
+
+}
+
+right.addEventListener(click, changeRight);
+left.addEventListener(click, changeLeft);
